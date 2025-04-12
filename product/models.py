df3e98fce9b41830c16ffc82ed059d9b8e11b34f
MohammadAmin <a.normohammadi83@gmail.com>
2025-04-13 02:25:06 +0330

    add session

--- product/models.py
+++ product/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 
+
 class Size(models.Model):
     title = models.CharField(max_length=10)
 
@@ -27,3 +28,11 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class Information(models.Model):
+    product = models.ForeignKey(Product,null=True, on_delete=models.CASCADE , related_name="informations")
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text[:40]
