c436ee52fe9a476c4ed6bbd04f683ac96ce21644
MohammadAmin <a.normohammadi83@gmail.com>
2025-04-13 14:52:49 +0330

    create cart module

--- cart/urls.py
+++ cart/urls.py
@@ -5,4 +5,5 @@ app_name = "cart"
 
 urlpatterns = [
     path('detail' , views.cart_detail_view, name='cart_detail'),
+    path('add<int:pk>' , views.cart_add_view, name='cart_add'),
 ]
--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect ,get_object_or_404
 from django.views import View
+from product.models import Product
 
 
 def cart_detail_view(request):
     if request.method == 'GET':
-        return render(request, "cart/cart_detail.html")
+        return render(request, "cart/cart_detail.html" , {})
+
+
+
+def cart_add_view(request , pk):
+    if request.method == 'POST':
+        product = get_object_or_404(Product, id=pk)
+        size , color , quantity = request.POST.get('size'), request.POST.get('color') , request.POST.get('quantity')
+        print(size , color , quantity)
+        return redirect("cart:cart_detail")
