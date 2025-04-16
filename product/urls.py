9c23ba8ca96cc44498dab7ec823a442a1079fd8a
MohammadAmin <a.normohammadi83@gmail.com>
2025-04-16 20:59:32 +0330

    updating category

--- product/urls.py
+++ product/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'product'
 urlpatterns = [
     path('<int:pk>/', views.product_detail_view, name='product-detail'),
+    path('navbar' , views.navbar_partial , name='navbar'),
 ]
--- product/views.py
+++ product/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 
 from django.shortcuts import render, get_object_or_404
-from .models import Product
+from .models import Product, Category
+
 
 def product_detail_view(request, pk):
     product = get_object_or_404(Product, pk=pk)
     return render(request, 'product/product_detail.html', {'product': product})
+
+def navbar_partial(request):
+    categories = Category.objects.all()
+    context = {'categories': categories}
+    return render(request, 'includes/navbar.html', context)
+
+def category_list(request):
+    categories = Category.objects.all()
+    context = {'categories': categories}
+    return render(request, 'category.html', context)
